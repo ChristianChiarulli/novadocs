db750f540c23ee6fade783f57c91503ddc5da6eb
Christian Chiarulli <chris.machine@pm.me>
2023-05-28 03:27:17 -0400

    feat: update links for contributing

--- src/settings.ts
+++ src/settings.ts
@@ -1,10 +1,31 @@
 export const SITE = {
-	title: 'NovaDocs',
-	description: 'Your website description.',
-	defaultLanguage: 'en-us',
-} as const;
+  title: "NovaDocs",
+  description: "Your website description.",
+  defaultLanguage: "en-us",
+};
 
-export const GITHUB_EDIT_URL = `https://github.com/ChristianChiarulli/novadocs/tree/master/examples/docs`;
+export const CONTRIBUTE = [
+  {
+    title: "Contributor Guide",
+    icon: "mdi:book-open",
+    link: "https://twitter.com/chrisatmachine",
+  },
+  {
+    title: "Edit this page",
+    icon: "mdi:lead-pencil",
+    link: "https://github.com/ChristianChiarulli/novadocs/tree/master/src/content/docs",
+  },
+  {
+    title: "Report an issue",
+    icon: "mdi:alert-circle-outline",
+    link: "https://discord.gg/8KvA3JU",
+  },
+];
 
-export const COMMUNITY_INVITE_URL = `https://astro.build/chat`;
+export const GIT_REPO = {
+  editURL: "https://github.com/ChristianChiarulli/novadocs/edit/master/src/content/docs",
+  contributingURL:
+    "https://github.com/ChristianChiarulli/novadocs/edit/master/CONTRIBUTING.md",
+  issueURL: "https://github.com/ChristianChiarulli/novadocs/issues",
+};
 
